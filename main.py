de39c739210d5f98dbd1d89f09fac2ab058110f3
Shivam singh <64598920+Programmer118@users.noreply.github.com>
2022-12-14 17:29:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import os
 import threading
 import time
 
+print('█░░░█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀   ▀▀█▀▀ █▀▀█   █▀▀█ █░░█ █▀▀ █░░█ █▀▀█ ▀▀█▀▀')
+print('█▄█▄█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀   ░░█░░ █░░█   █░░█ █▄▄█ █░░ █▀▀█ █▄▄█ ░░█░░')
+print('░▀░▀░ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀   ░░▀░░ ▀▀▀▀   █▀▀▀ ▄▄▄█ ▀▀▀ ▀░░▀ ▀░░▀ ░░▀░░')
+print('\t\t\t\t\t\t\t\tBy Shivam Singh')
+
+print("{If you want to run on internet  go to  ngrok folder (you have to install ngrok)}")
 
 def main():
 
-    print('█░░░█ █▀▀ █░░ █▀▀ █▀▀█ █▀▄▀█ █▀▀   ▀▀█▀▀ █▀▀█   █▀▀█ █░░█ █▀▀ █░░█ █▀▀█ ▀▀█▀▀')
-    print('█▄█▄█ █▀▀ █░░ █░░ █░░█ █░▀░█ █▀▀   ░░█░░ █░░█   █░░█ █▄▄█ █░░ █▀▀█ █▄▄█ ░░█░░')
-    print('░▀░▀░ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀░░░▀ ▀▀▀   ░░▀░░ ▀▀▀▀   █▀▀▀ ▄▄▄█ ▀▀▀ ▀░░▀ ▀░░▀ ░░▀░░')
-    print('\t\t\t\t\t\t\t\tBy Shivam Singh')
 
-    print("{If you want to run on internet  go to  ngrok folder (you have to install ngrok)}")
-
-    print("\n1: Start Server...")
-    print("2: Start Client to Connect to the server...")
-    print("3: Exit...\n\n")
+    print("\n\t\t\t\t1: Start Server...")
+    print("\t\t\t\t2: Start Client to Connect to the server...")
+    print("\t\t\t\t3: Exit...\n\n")
 
     check_user_input = input("Select [1], [2] or [3]: ")
 
     if check_user_input=='1':
 
         t1 = threading.Thread(target=os.system,args=("python chat_server.pyw",),daemon=True)
+        time.sleep(4)
         os.system("start chat_client.py",)
         t1.start()
         time.sleep(2)
@@ -29,10 +30,12 @@ def main():
     elif check_user_input=='2':
 
         os.system('start chat_client.py')
+       
 
     elif check_user_input=='3':
 
         os.system("taskkill /f /im python.exe >> nul")
+       
 
     else:
 
